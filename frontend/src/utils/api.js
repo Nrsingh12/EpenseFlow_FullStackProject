35e54db820b97eb049bd88ebca3d58d1b5a81430
Nishant Ranjan Singh <nishantranjansingh12@gmail.com>
2025-12-08 12:52:36 +0530

    Upadete code for Deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://epenseflow-fullstackproject.onrender.com';
 
 let authToken = null;
 
