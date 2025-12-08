fc9097dd8c64bc2ab18476328732206dfc2d70ff
Nishant Ranjan Singh <nishantranjansingh12@gmail.com>
2025-12-08 12:15:00 +0530

    Enhance expense detail page with improved UI

--- frontend/src/pages/ExpenseDetail.js
+++ frontend/src/pages/ExpenseDetail.js
@@ -87,9 +87,9 @@ const ExpenseDetail = () => {
   };
 
   const formatCurrency = (amount) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD'
+      currency: 'INR'
     }).format(amount);
   };
 
